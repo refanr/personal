96ad9804738a534d3460a4831e31e5e2e529bf6b
Reynir Gunnarsson <reynirg20@ru.is>
2024-03-25 21:21:37 +0000

    colors and pass pic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
+
 function App() {
   return (
     <>
       <h1>Reynir Gunnarsson</h1>
+      <div className="mynd"></div>
+      <a href="http://www.google.com">Google</a>
     </>
   );
 }
